4807767a0e26d1540b7db65e4f30c52e5e3704f8
artyom.burdavitsyn <artyom.burdavitsyn@gmail.com>
2019-06-26 14:47:10 +0400

    Added ex5

--- Блок 5/ex5.3-24.py
+++ Блок 5/ex5.3-24.py
@@ -3,3 +3,54 @@
 #имеющей строк N и столбцов M. Дан номер строки L и номер
 #столбца K, при помощи которых исходная матрица разбивается на
 #четыре части. Найти сумму элементов каждой части.
+
+import numpy as np
+
+n = int(input('n = '));
+m = int(input('m = '));
+
+a = np.empty ((n, m), dtype = float);
+
+for i in range(n):
+    for j in range(m):
+        a[i, j] = int(input());
+
+print(a);
+
+k = int(input('k = '));
+l = int(input('l = '));
+
+if(k<=n and l<=m):
+    sum = 0;
+    #сумма элементов слева сверху
+    for i in range(0, k):
+        for j in range(0, l):
+            sum += a[i, j];
+
+    print('Cумма элементов слева сверху', sum);
+
+    sum = 0;
+    #сумма элементов справа сверху
+    for i in range(0, k):
+        for j in range(l, m):
+            sum += a[i, j];
+
+    print('Cумма элементов справа сверху', sum);
+
+    sum = 0;
+    #сумма элементов слева снизу
+    for i in range(k, n):
+        for j in range(0, l):
+            sum += a[i, j];
+
+    print('Cумма элементов слева снизу', sum);
+
+    sum = 0;
+    #сумма элементов справа снизу
+    for i in range(k, n):
+        for j in range(l, m):
+            sum += a[i, j];
+
+    print('Cумма элементов справа снизу', sum);
+else:
+    print('Введены неверные значения');
